b2fb0ce1d18050d883af7a4167eedcae8544a496
pa.sheludyakov <pasha.technician@bk.ru>
2025-09-20 21:25:33 +0300

    Добавлен репозиторий MessengerMicro

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name = "CommunicatorInfrastructure"
 includeBuild("component/AuthMicro")
 includeBuild("component/NetworkingMicro")
 includeBuild("component/WebSocketHub")
+includeBuild("component/MessengerMicro")
